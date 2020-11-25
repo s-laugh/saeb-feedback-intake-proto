aae2f40654f3a1da80b2b04db638ee66eefd50b7
Shaun Laughland <shaun.laughland@hrsdc-rhdcc.gc.ca>
2020-11-25 09:34:55 -0500

    installing loopback

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {FeedbackIntake} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new FeedbackIntake({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: FeedbackIntake;
+  client: Client;
+}
